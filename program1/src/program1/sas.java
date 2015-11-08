32a13739fb0430c68cbed1328ea41216fc29e628
ahsankhan <FA12-BSE-099@LAB7B-10.cns.ciit>
2015-11-09 00:06:00 +0500

    Fuck

--- program1/src/program1/sas.java
+++ program1/src/program1/sas.java
@@ -2,7 +2,7 @@ package program1;
 
 public class sas {
 	public static void main(String[] args) {
-        // TODO code application logic here
-		System.out.println("sasa");
+        
+		System.out.println(Fu Ahsan");
     }
 }
